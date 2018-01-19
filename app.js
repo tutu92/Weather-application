0d6e1f54f1c2645f47d3d5e7bb431e193eb1775d
Tuomas Lampinen <tuu-mas@hotmail.com>
2018-01-19 13:53:43 +0000

    update dburl

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 
-// mongoose.connect("mongodb://localhost/temperatures", {useMongoClient: true});
-mongoose.connect("mongodb://Tuomas:password@ds263137.mlab.com:63137/weatherapp", {useMongoClient: true});
+console.log(process.env.DATABASEURL);
+
+mongoose.connect(process.env.DATABASEURL, {useMongoClient: true});
+// mongoose.connect("mongodb://Tuomas:password@ds263137.mlab.com:63137/weatherapp", {useMongoClient: true});
 
 app.locals.moment = require("moment");
 
